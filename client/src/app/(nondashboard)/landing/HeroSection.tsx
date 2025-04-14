ea06272b702905148e209854bb4c8565c0df92db
Paulo Cesar <paulocgsantos99@gmail.com>
2025-04-14 10:14:47 -0300

    feat: add functionalities

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -1,11 +1,49 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useDispatch } from 'react-redux'
+import { useRouter } from 'next/navigation'
+import { setFilters } from '@/state'
 
 const HeroSection = () => {
+  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleLocationSearch = async () => {
+    try {
+      const trimerdQuery = searchQuery.trim();
+      if(!trimerdQuery) return;
+
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(trimerdQuery)}.json?access_token=${
+        process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+      const data = await response.json();
+      if(data.features && data.features.length > 0){
+        const [lng, lat] = data.features[0].center;
+        dispatch(
+          setFilters({
+            location: trimerdQuery,
+            coordinates: [lat, lng]
+          })
+        );
+        const params = new URLSearchParams({
+          location: trimerdQuery,
+          lat: lat.toString(),
+          lng: lng.toString()
+        });
+        router.push(`/search?${params.toString()}`)
+      }
+    } catch (error) {
+      console.log("error search location", error);
+    }
+  }
+
   return (
     <div className='relative h-screen'>
       {/* Imagem de fundo */}
@@ -37,10 +75,14 @@ const HeroSection = () => {
           <div className='flex justify-center'>
             <Input 
             type='text'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             placeholder='Pesquise a cidade, bairro e rua'
             className='w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12'
             />
-            <Button className='bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12'>
+            <Button 
+            onClick={handleLocationSearch}
+            className='bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12'>
                 Pesquisar
             </Button>
           </div>
